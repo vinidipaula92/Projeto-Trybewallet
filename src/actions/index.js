1a48e69a358b7795f5ef316fcef0e083e3deb276
vinidipaula <vinidipaula92@gmail.com>
2022-03-31 17:33:09 -0300

    Requisito 1 finalizado

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,4 @@
 // Coloque aqui suas actions
+const login = (value) => ({ type: 'LOGIN', value });
+
+export default login;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,66 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import login from '../actions/index';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      senha: '',
+    };
+  }
+
+  handleDisable = () => {
+    const { email, senha } = this.state;
+    const seis = 6;
+    if (!email.includes('.com' || '@') || senha.length < seis) {
+      return true;
+    }
+    return false;
+  }
+
   render() {
-    return <div>Login</div>;
+    const { loginSucess } = this.props;
+    const { email, senha } = this.state;
+    return (
+      <div>
+        <h1>Trybe Wallet - Login</h1>
+        <input
+          type="email"
+          placeholder="Email"
+          data-testid="email-input"
+          onChange={ (e) => this.setState({ email: e.target.value }) }
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          data-testid="password-input"
+          onChange={ (e) => this.setState({ senha: e.target.value }) }
+        />
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ this.handleDisable() }
+            onClick={ () => loginSucess(email, senha) }
+          >
+            Entrar
+
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  loginSucess: (email, senha) => dispatch(login(email, senha)),
+});
+
+Login.propTypes = {
+  loginSucess: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
